897be01688376400d44755d47ca213c3c19d6f80
ftoralesacosta <ftoralesacosta@lbl.gov>
2025-05-30 14:29:25 -0700

    appended scattered Electron to event dataset

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -10,16 +10,29 @@ from sklearn.utils import shuffle
 from icecream import ic
 ic.configureOutput(includeContext=True)
 
-'''particles = [pT, eta, phi, PID, z]'''
-'''NO JET dateset. These are all particles EIC events'''
 
 PID_INDEX = 9
 
-labels = {
-    'small_test_file.h5'
-    # 'pythia8NCDIS_18x275_minQ2=100.h5'
+labels = { #file names
+    # 'small_test_file.h5'
+    'pythia8NCDIS_18x275_minQ2=100.h5'
 }
 
+# Hardcoded pid booleans
+neutral_hads = [
+    111,   # pi0
+    2112,  # neutron
+    130,   # K0L
+    310,   # K0S
+    311,   # K0
+    3122,  # Lambda0
+]
+
+charged_hads = [
+    211,    # pi±
+    321,    # K±
+    2212    # p / p̄
+]  
 
 
 def process(p, gen_ele):
@@ -29,90 +42,66 @@ def process(p, gen_ele):
     And provide PID boolean features for each particle. Note, we do not save charge ATM 5/29/25
      
     A key pre-processing step of mean->0.0 and stdev->1.0 is done in utils.py
+
     In dis_root_to_h5.py, the leading electron is set as the first particle in the array
-
-    If the first particle is not an electron, that means it was not reconstructed.
-    If that is the case, we normalize reco particles by the Gen. electron here
+    If the first reco-particle is not an electron, that means one was not reconstructed.
+    If that is the case, we normalize reco particles by the Gen. electron here (gen_ele)
     Vinny's idea is to then train a classifier at gen-level to determine if electron is reconstructed
-
-    gen_ele is Gen. Level electron
     '''
 
-    #make a deep copy of electron eta and P to leave original un-modified
-    #we modify these new arrays to be reco or Gen electron
-    e_eta = p[:,0,-2].copy()
-    e_P = p[:,0,0].copy()
+    # Return Feature Dimensions
+    N_Ele_Feat = 6     # eta, phi, E, xyz-vert
+    N_Part_Feat = 11   # ^ + five PID booleans
 
-    mask = np.abs(p[:,0,PID_INDEX]) == 11  # mask to see if first particle is electron
-    e_eta = np.where(mask, e_eta, gen_ele[:,-2])  # set e_eta to gen_ele_eta if MASK=0
-    e_P   = np.where(mask, e_P,   gen_ele[:,0])   # set e_P to gen_ele_P if MASK=0
+    electrons = p[:,0,:]
+    mask = np.abs(electrons[:,PID_INDEX]) == 11   # mask if first particle is actually electron
+    ic(np.shape(mask[:,np.newaxis]))
+    ic(np.shape(electrons))
+    ic(np.shape(gen_ele))
+    ic(np.shape(p))
+    electrons = np.where(mask[:, np.newaxis], electrons, gen_ele)
 
-    #log(P_rel)
+
+    # Get log(P_rel)
+    e_P = electrons[:,0]
     P_rel = np.ma.masked_invalid(np.divide(p[:, 1:, 0], e_P[:, np.newaxis]))
     log_P_rel = np.ma.log(P_rel)
     log_P_rel = log_P_rel.filled(0)
 
     #new particles array excludes first particle (electron)
-    new_p = np.zeros(shape=(p.shape[0],p.shape[1]-1,11))
+    new_p = np.zeros(shape=(p.shape[0],p.shape[1]-1, N_Part_Feat))
     
-    new_p[:,:,0] = p[:,1:,-2] + e_eta[:, np.newaxis] # eta rel
-    new_p[:,:,1] = p[:,1:,-1]         # phi
-    new_p[:,:,2] = log_P_rel
-    new_p[:,:,3] = p[:,1:,4]         # x-vert.
-    new_p[:,:,4] = p[:,1:,5]         # y-vert.
-    new_p[:,:,5] = p[:,1:,6]         # z-vert.nt 
+    # new_p[:,:,0] = p[:,1:,-2] + e_eta[:, np.newaxis] # eta rel
+    new_p[:,:,0] = p[:,1:,-2] + electrons[:,-2][:, np.newaxis]      # eta rel
+    new_p[:,:,1] = p[:,1:,-1]                                       # phi
+    new_p[:,:,2] = log_P_rel                                        # log(P_rel)
+    new_p[:,:,3] = p[:,1:,4]  + electrons[:,4][:, np.newaxis]       # x-vert.
+    new_p[:,:,4] = p[:,1:,5]  + electrons[:,5][:, np.newaxis]       # y-vert.
+    new_p[:,:,5] = p[:,1:,6]  + electrons[:,6][:, np.newaxis]       # z-vert.
 
-
-    #hardcoded pid booleans
-    neutral_hads = [
-        111,   # pi0
-        2112,  # neutron
-        130,   # K0L
-        310,   # K0S
-        311,   # K0
-        3122,  # Lambda0
-    ]
-
-    charged_hads = [
-        211,    # pi±
-        321,    # K±
-        2212    # p / p̄
-    ]  
-
-    new_p[:,:,6] = np.abs(p[:,1:,PID_INDEX]) == 11. #electron
-    new_p[:,:,7] = np.abs(p[:,1:,PID_INDEX]) == 13. #muon
-    new_p[:,:,8] = np.abs(p[:,1:,PID_INDEX]) == 22. #photon
+    new_p[:,:,6] = np.abs(p[:,1:,PID_INDEX]) == 11.                 # is electron
+    new_p[:,:,7] = np.abs(p[:,1:,PID_INDEX]) == 13.                 # is muon
+    new_p[:,:,8] = np.abs(p[:,1:,PID_INDEX]) == 22.                 # is photon
     new_p[:,:,9] = np.isin(np.abs(p[:,1:,PID_INDEX]).astype(int), neutral_hads)  # is neutral hadron
     new_p[:,:,10] = np.isin(np.abs(p[:,1:,PID_INDEX]).astype(int), charged_hads) # is charged hadron
 
 
-    ic(new_p[1:3])
+    # ic(new_p[1:3])
     new_p = new_p*mask[:,np.newaxis,np.newaxis]
     print("\n\nLine 84, after MASK\n")
-    ic(new_p[1,:3])
-    # print(new_p[:2])
+    # ic(new_p[1,:3])
 
 
     #Now Edit electron, before returning
-    e_phi = np.where(mask, p[:,0,-1], gen_ele[:,-1])
-    e_vx   = np.where(mask, p[:,0,4],   gen_ele[:,4])
-    e_vy   = np.where(mask, p[:,0,5],   gen_ele[:,5])
-    e_vz   = np.where(mask, p[:,0,6],   gen_ele[:,6])
-    e_is_el   = np.ones(len(gen_ele))
-    e_is_mu   = np.zeros(len(gen_ele))
-    e_is_pho   = np.zeros(len(gen_ele))
-    e_is_nhad   = np.zeros(len(gen_ele))  # is neutral_hadron
-    e_is_chad   = np.zeros(len(gen_ele))
+    new_ele = np.zeros(shape=(electrons.shape[0], N_Ele_Feat)) #[Nevt, Nfeat]
+    new_ele[:,0] = electrons[:,-2]
+    new_ele[:,1] = electrons[:,-1]
+    new_ele[:,2] = electrons[:, 0]
+    new_ele[:,3] = electrons[:, 4]
+    new_ele[:,4] = electrons[:, 5]
+    new_ele[:,5] = electrons[:, 6]
 
-    features = [
-        e_eta, e_phi, e_P,
-        e_vx, e_vy, e_vz,
-        e_is_el, e_is_mu, e_is_pho,
-        e_is_nhad, e_is_chad
-    ]
-    electrons = np.stack(features, axis=1)
-
-    return new_p, electrons
+    return new_p, new_ele
 
 
 def preprocess(path, labels, nevent_max=-1, npart_max=-1):
@@ -144,30 +133,30 @@ def preprocess(path, labels, nevent_max=-1, npart_max=-1):
 
             ntotal = h5f['reco_particle_features'][:nevent_max].shape[0]
 
+            reco_particles = h5f['reco_particle_features'][:nevent_max,:npart_max].astype(np.float32)
+            reco_events = h5f['reco_InclusiveKinematicsESigma'].astype(np.float32)
+
             gen_particles = h5f['gen_particle_features'][:nevent_max,:npart_max].astype(np.float32)
             gen_events = h5f['gen_InclusiveKinematicsTruth'].astype(np.float32)
 
             gen_electrons = gen_particles[:,0,:]
-            ic(np.shape(gen_particles))
-            ic(np.shape(gen_electrons))
 
-            gen_particles, gen_electrons = process(gen_particles, gen_electrons)
-
-            ic(np.shape(gen_particles))
-            ic(np.shape(gen_electrons))
-
-
-            reco_particles = h5f['reco_particle_features'][:nevent_max,:npart_max].astype(np.float32)
-            reco_events = h5f['reco_InclusiveKinematicsESigma'].astype(np.float32)
-
-            ic(np.shape(reco_particles))
+            ic("Before Process", np.shape(reco_particles))
             reco_particles, reco_electrons = process(reco_particles, gen_electrons)
-            ic(np.shape(reco_particles))
+            ic("After Process", np.shape(reco_particles))
 
 
+            ic("Before Process", np.shape(gen_particles))
+            gen_particles, gen_electrons = process(gen_particles, gen_electrons)
+            ic("After Process", np.shape(gen_particles))
+
+            ic(np.shape(gen_events))
+            gen_events = np.hstack((gen_events, gen_electrons))
+            ic(np.shape(gen_events))
+            reco_events = np.hstack((reco_events, reco_electrons))
+
+            # Train, Val, test Split
             datasets = {'train': train, 'val': val, 'test': test}
-
-
             splits = { 'train': (0, 0.63), 'val':   (0.63, 0.7), 'test':  (0.7, 1.0) }
 
             for split_name, (start_frac, end_frac) in splits.items():
@@ -179,23 +168,6 @@ def preprocess(path, labels, nevent_max=-1, npart_max=-1):
                     [gen_particles, reco_particles, gen_events, reco_events]
                 ):
                     datasets[split_name][key].append(data[start:end])
-
-            # train['gen_particles'].append(gen_particles[:int(0.63*ntotal)])
-            # train['reco_particles'].append(reco_particles[:int(0.63*ntotal)])
-            # train['gen_events'].append(gen_events[:int(0.63*ntotal)])
-            # train['reco_events'].append(reco_events[:int(0.63*ntotal)])
-
-            # val['gen_particles'].append(gen_particles[int(0.63*ntotal):int(0.7*ntotal)])
-            # val['reco_particles'].append(reco_particles[int(0.63*ntotal):int(0.7*ntotal)])
-            # val['gen_events'].append(gen_events[int(0.63*ntotal):int(0.7*ntotal)])
-            # val['reco_events'].append(reco_events[int(0.63*ntotal):int(0.7*ntotal)])
-
-            # test['gen_particles'].append(gen_particles[int(0.7*ntotal):])
-            # test['reco_particles'].append(reco_particles[int(0.7*ntotal):])
-            # test['gen_events'].append(gen_events[int(0.7*ntotal):])
-            # test['reco_events'].append(reco_events[int(0.7*ntotal):])
-
-
             # FIXME: 5/29: make sure we don't just pull the 200GB dataset into memory all at once...
 
 
@@ -231,23 +203,23 @@ if __name__=='__main__':
     parser.add_option("--npart_max", type="int", default='200', help="max number of particses per event")
     (flags, args) = parser.parse_args()
 
-    train, val, test = preprocess(os.path.join(flags.folder, 'parnassus_TrainTestVal'),labels, flags.nevent_max, flags.npart_max)
+    train, val, test = preprocess(os.path.join(flags.folder, 'h5_files'),labels, flags.nevent_max, flags.npart_max)
 
-    with h5.File('{}/train_{}.h5'.format(os.path.join(flags.folder, 'parnassus_TrainTestVal'),"eic"), "w") as fh5:
+    with h5.File('{}/train_{}.h5'.format(os.path.join(flags.folder, 'h5_files'),"eic"), "w") as fh5:
         dset = fh5.create_dataset('gen_particles', data=train['gen_particles'])
         dset = fh5.create_dataset('gen_events', data=train['gen_events'])
         dset = fh5.create_dataset('reco_particles', data=train['reco_particles'])
         dset = fh5.create_dataset('reco_events', data=train['reco_events'])
 
 
-    with h5.File('{}/test_{}.h5'.format(os.path.join(flags.folder, 'parnassus_TrainTestVal'),"eic"), "w") as fh5:
+    with h5.File('{}/test_{}.h5'.format(os.path.join(flags.folder, 'h5_files'),"eic"), "w") as fh5:
         dset = fh5.create_dataset('gen_particles', data=test['gen_particles'])
         dset = fh5.create_dataset('gen_events', data=test['gen_events'])
         dset = fh5.create_dataset('reco_particles', data=test['reco_particles'])
         dset = fh5.create_dataset('reco_events', data=test['reco_events'])
 
 
-    with h5.File('{}/val_{}.h5'.format(os.path.join(flags.folder, 'parnassus_TrainTestVal'),"eic"), "w") as fh5:
+    with h5.File('{}/val_{}.h5'.format(os.path.join(flags.folder, 'h5_files'),"eic"), "w") as fh5:
         dset = fh5.create_dataset('gen_particles', data=val['gen_particles'])
         dset = fh5.create_dataset('gen_events', data=val['gen_events'])
         dset = fh5.create_dataset('reco_particles', data=val['reco_particles'])
